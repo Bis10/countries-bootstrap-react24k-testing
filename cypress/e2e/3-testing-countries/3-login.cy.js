871f4df5d165d511ba63844733fa17f87b54b63c
Suyel Bishnu <suyel2007@gmail.com>
2024-10-05 18:20:22 +0300

    Update the login.cy.js

--- cypress/e2e/3-testing-countries/3-login.cy.js
+++ cypress/e2e/3-testing-countries/3-login.cy.js
@@ -1,12 +1,11 @@
-describe("Login page load test", () => {
-  it("should load the Login page successfully", () => {
+describe("Login", () => {
+  it("Log in user", () => {
     // Visit the Login page
-    cy.visit("http://localhost:5173/login"); 
+    cy.visit("http://localhost:5173/login");
 
     // Check that the page contains the input fields and buttons
-    cy.get('input[type=email]').type("test@test.com");
-    cy.get('input[type=password]').type("12345");
-    cy.get("button").contains("Login").should("be.visible");
-    cy.get("button").contains("Don't have an account?").should("be.visible");
+    cy.get("input[type=email]").type("test@test.com");
+    cy.get("input[type=password]").type("Abcd1234");
+    cy.get("button").contains("Login").click();
   });
 });
